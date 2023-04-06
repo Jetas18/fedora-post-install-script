2e34aa8c8be775cf7cd6533eecd264934d9ece99
Jetas18 <128137931+Jetas18@users.noreply.github.com>
2023-04-06 12:32:34 +0530

    Update and rename fedora to fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,19 +1,28 @@
+#!/bin/bash
+
 sudo sh -c echo fastestmirror=True >> /etc/dnf/dnf.conf
 sudo sh -c echo max_parallel_downloads=5 >> /etc/dnf/dnf.conf
 sudo sh -c echo default=True >> /etc/dnf/dnf.conf
 sudo sh -c echo keepcache=Faslse >> /etc/dnf/dnf.conf
+sleep 3
 sudo dnf update
+sleep 5
 sudo dnf upgrade
+sleep 5
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf groupupdate core
+sleep 5
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sleep 5
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf groupupdate sound-and-video
+sleep 5
 sudo dnf install -y gcc-c++ make cmake automake autoconf libtool pkg-config \
   libpng-devel libjpeg-devel libtiff-devel zlib-devel libwebp-devel \
   libjpeg-turbo-devel libicu-devel libxml2-devel gettext-devel libcurl-devel \
   openssl-devel readline-devel ncurses-devel sqlite-devel libuuid-devel
-sudo dnf install -y git wget curl htop vim
+sleep 10
+sudo dnf install -y wget curl htop vim
 git clone --recursive https://github.com/andresgongora/synth-shell.git
 chmod +x synth-shell/setup.sh
 cd synth-shell
