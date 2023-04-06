55a6275fad3e037727029cb2d8eab9638e4eb8c1
Jetas18 <128137931+Jetas18@users.noreply.github.com>
2023-04-06 20:42:37 +0530

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
-x=1
-sudo sh -c "echo fastestmirror=True >> /etc/dnf/dnf.conf"
-sudo sh -c "echo max_parallel_downloads=5 >> /etc/dnf/dnf.conf"
-sudo sh -c "echo default=True >> /etc/dnf/dnf.conf"
-sudo sh -c "echo keepcache=False >> /etc/dnf/dnf.conf"
-if [[ $x == 1 ]]
-then
-    read -p "press enter to continue with the setup"
-    sudo dnf update
-    sudo dnf upgrade
-    sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-    sudo dnf groupupdate core
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-    sudo dnf groupupdate sound-and-video
-    sudo dnf install -y gcc-c++ make cmake automake autoconf libtool pkg-config \
-    libpng-devel libjpeg-devel libtiff-devel zlib-devel libwebp-devel \
-    libjpeg-turbo-devel libicu-devel libxml2-devel gettext-devel libcurl-devel \
-    openssl-devel readline-devel ncurses-devel sqlite-devel libuuid-devel wget curl htop vim
-    git clone --recursive https://github.com/andresgongora/synth-shell.git
-    chmod +x synth-shell/setup.sh
-    cd synth-shell
-    ./setup.sh
-fi
+cat << EOF | sudo tee -a /etc/dnf/dnf.conf
+fastestmirror=True
+max_parallel_downloads=5
+default=True
+keepcache=True
+EOF
+read -p "press enter to continue with the setup"
+sudo dnf update && sudo dnf upgrade || exit 1
+sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm || exit 1
+sudo dnf groupupdate core || exit 1
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo ||exit 1
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin || exit 1
+sudo dnf groupupdate sound-and-video || exit 1
+sudo dnf install -y gcc-c++ make cmake automake autoconf libtool pkg-config \
+libpng-devel libjpeg-devel libtiff-devel zlib-devel libwebp-devel \
+libjpeg-turbo-devel libicu-devel libxml2-devel gettext-devel libcurl-devel \
+openssl-devel readline-devel ncurses-devel sqlite-devel libuuid-devel wget curl htop vim
+sudo dnf install snapd || exit 1
+sudo ln -s /var/lib/snapd/snap /snap
+reboot
 
 
